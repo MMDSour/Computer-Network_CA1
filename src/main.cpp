133e34da2b3e0c4b34ce681a68d9b736d213db76
MMDSour <soor.mmd81@gmail.com>
2024-10-22 19:06:40 +0330

    run the qml with Q qml Application

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
-
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
+    QGuiApplication app(argc, argv);
 
+    QQmlApplicationEngine engine;
+    engine.load(QUrl::fromLocalFile("../../Main.qml"));
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
